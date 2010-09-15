3f60f8ace67c1e6365e0af8b4344a037ccc10911
Steve Malers <steve.malers@openwaterfoundation.org>
2010-09-15 21:16:52 +0000

    Increase the number of filters from 3 to 6 and make the choices editable to allow more flexibility.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/RiversideDB_DMI/java_142/trunk@56 a50ff48b-d70d-9343-9628-e698d507aa0b

--- src/RTi/DMI/RiversideDB_DMI/RiversideDB_MeasTypeMeasLocGeoloc_InputFilter_JPanel.java
+++ src/RTi/DMI/RiversideDB_DMI/RiversideDB_MeasTypeMeasLocGeoloc_InputFilter_JPanel.java
@@ -47,57 +47,61 @@ public RiversideDB_MeasTypeMeasLocGeoloc_InputFilter_JPanel(RiversideDBDataStore
 	List<String> measTypeSubTypeList = dmi.getMeasTypeSubTypeList();
 	List<String> measTypeUnitsList = dmi.getMeasTypeUnitsList();
 	
+	// Choices are editable to allow multiple choices to be matched with string matching filter options
+	
 	// Omit Geoloc.country for now - typically will be in one country
 	filters.add(new InputFilter("Data Source Abbreviation",
         measTypeTableName + "source_abbrev" + rd, "source_abbrev",
-        StringUtil.TYPE_STRING, measTypeDataSourceAbbrevList, measTypeDataSourceAbbrevList, false));
+        StringUtil.TYPE_STRING, measTypeDataSourceAbbrevList, measTypeDataSourceAbbrevList, true));
 	/*
     filters.add(new InputFilter("Data Type (Main)",
         measTypeTableName + "data_type" + rd, "data_type",
-        StringUtil.TYPE_STRING, measTypeDataTypeList, measTypeDataTypeList, false));
+        StringUtil.TYPE_STRING, measTypeDataTypeList, measTypeDataTypeList, true));
         */
     filters.add(new InputFilter("Data Sub-type",
         measTypeTableName + "sub_type" + rd, "sub_type",
-        StringUtil.TYPE_STRING, measTypeSubTypeList, measTypeSubTypeList, false));
+        StringUtil.TYPE_STRING, measTypeSubTypeList, measTypeSubTypeList, true));
     filters.add(new InputFilter("Data Units",
         measTypeTableName + "units_abbrev" + rd, "units_abbrev",
-        StringUtil.TYPE_STRING, measTypeUnitsList, measTypeUnitsList, false));
+        StringUtil.TYPE_STRING, measTypeUnitsList, measTypeUnitsList, true));
     filters.add(new InputFilter("Description",
         measTypeTableName + "description" + rd, "description",
-        StringUtil.TYPE_STRING, null, null, false));
+        StringUtil.TYPE_STRING, null, null, true));
     filters.add(new InputFilter("Scenario",
         measTypeTableName + "scenario" + rd, "scenario",
-        StringUtil.TYPE_STRING, null, null, false));
+        StringUtil.TYPE_STRING, null, null, true));
     filters.add(new InputFilter("Station County",
        geolocTableName + "county" + rd, "county", StringUtil.TYPE_STRING,
-       geolocCountyList, geolocCountyList, false));
+       geolocCountyList, geolocCountyList, true));
     filters.add(new InputFilter("Station Elevation",
         geolocTableName + "elevation" + rd, "elevation",
-        StringUtil.TYPE_DOUBLE, null, null, false));
+        StringUtil.TYPE_DOUBLE, null, null, true));
     filters.add(new InputFilter("Station Latitude",
         geolocTableName + "latitude" + rd, "latitude",
-        StringUtil.TYPE_DOUBLE, null, null, false));
+        StringUtil.TYPE_DOUBLE, null, null, true));
     filters.add(new InputFilter("Station Longitude",
         geolocTableName + "longitude" + rd, "longitude",
-        StringUtil.TYPE_DOUBLE, null, null, false));
+        StringUtil.TYPE_DOUBLE, null, null, true));
     filters.add(new InputFilter("Station State",
         geolocTableName + "state" + rd, "state", StringUtil.TYPE_STRING,
-        geolocStateList, geolocStateList, false));
-    filters.add(new InputFilter("Station Identifier",
+        geolocStateList, geolocStateList, true));
+    filters.add(new InputFilter("Station Identifier (ID)",
         measLocTableName + "identifier" + rd, "identifier", 
-        StringUtil.TYPE_STRING, null, null, false));
+        StringUtil.TYPE_STRING, null, null, true));
     filters.add(new InputFilter("Station Name",
         measLocTableName + "measloc_name" + rd, "measloc_name", 
-        StringUtil.TYPE_STRING, null, null, false));
+        StringUtil.TYPE_STRING, null, null, true));
     filters.add(new InputFilter("Station X",
         geolocTableName + "x" + rd, "x",
-        StringUtil.TYPE_DOUBLE, null, null, false));
+        StringUtil.TYPE_DOUBLE, null, null, true));
     filters.add(new InputFilter("Station Y",
         geolocTableName + "y" + rd, "y",
-        StringUtil.TYPE_DOUBLE, null, null, false));
+        StringUtil.TYPE_DOUBLE, null, null, true));
 		
 	setToolTipText("<html>RiversideDB queries can be filtered based on station and time series metadata.</html>");
-	setInputFilters(filters, 3, 15);
+	// At the request of the NCWCD, use 6 filters
+	// TODO SAM 2010-09-13 may want to make the number of filters configurable
+	setInputFilters(filters, 6, 15);
 }
 
 /**
